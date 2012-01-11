b4a6cc396697ed60cb9644a98a6376138cbb232e
Brad Heller <brad@aboutus.org>
2012-01-11 10:18:01 -0800

    Initial commit.

---
+++ config/initializers/sass.rb
@@ -0,0 +1,14 @@
+Sass::Plugin.remove_template_location("./public/stylesheets/sass")
+Sass::Plugin.options.merge!(
+    :template_location => 'app/stylesheets',
+    :css_location => 'tmp/stylesheets'
+)
+
+if ['test', 'development'].include? Rails.env
+  Sass::Plugin.options[:always_update] = true
+  Sass::Plugin.options[:line_comments] = true
+end
+
+Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static', :urls => ['/stylesheets'], :root => "#{Rails.root}/tmp")
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Todo::Application.routes.draw do
+  root :to => 'todos#index'
+end
