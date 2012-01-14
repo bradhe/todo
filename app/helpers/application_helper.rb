3ca38bcc8b1ce40c791fb088ac754679d0f2dd1d
Brad Heller <brad.heller@gmail.com>
2012-01-14 00:44:56 -0800

    Eeeeyup, this is it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
   def mobile?
+  end
+
+  def iphone?
     request.user_agent.include?("iPhone")
+  end
+
+  def android?
+    request.user_agent.include?("Android")
   end
 
   def include_appcache?
